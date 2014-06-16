ebd9904935526728068428fb5aa7b84dbb074b77
Katie Brasfield <katiebrasfield@gmail.com>
2014-06-16 10:11:27 -0700

    first commit

--- lib/sort.rb
+++ lib/sort.rb
@@ -40,27 +40,6 @@ def merge(left, right)
   array2 + left + right
 end
 
-# def quicksort(array)
-#   return array if array.length <= 1
-
-#   pivot_index = (array.length / 2).to_i
-#   pivot_value = array[pivot_index]
-#   array.delete_at(pivot_index)
-
-#   lesser = Array.new
-#   greater = Array.new
-
-#   array.each do |x|
-#     if x <= pivot_value
-#       lesser << x
-#     else
-#       greater << x
-#     end
-#   end
-
-#   return quicksort(lesser) + [pivot_value] + quicksort(greater)
-# end
-
 def quickSort(array)
   length = array.length
 
