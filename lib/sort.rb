f7329a006b25d6be9a9ece309eef2db7e972dc0f
Katie Brasfield <katiebrasfield@gmail.com>
2014-06-16 15:14:38 -0700

    completed radix

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,168 +1,97 @@
+class Array
 
-def insertionSort(array)
-  (1...array.length).each do |x|
-    value = array.delete_at(x)
-    index = x
-    while index > 0 && value < array[index - 1]
-      index -= 1
-    end
-    array.insert(index, value)
-  end
-  return array
-end
-
-def mergeSort(array)
-  #puts "--> ARRAY: #{array.inspect}"
-  length = array.length
-  if length <= 1
-    return array
-  else
-    x = length/2
-    left = mergeSort(array[0...x])
-    right = mergeSort(array[x..length])
-    #puts "BEFORE MERGE LEFT: #{left.inspect} RIGHT: #{right.inspect}"
-    merge(left, right)
-  end
-end
-
-def merge(left, right)
-  array2 = []
-  until left.empty? == true || right.empty? == true
-    if left[0] <= right[0]
-      #puts "IF LEFT: #{left.inspect} RIGHT: #{right.inspect}"
-      array2.push(left.shift)
-    else
-      #puts "ELSE LEFT: #{left.inspect} RIGHT: #{right.inspect}"
-      array2.push(right.shift)
-    end
-    #puts "----> ARRAY2: #{array2.inspect}"
-  end
-  array2 + left + right
-end
-
-def quickSort(array)
-  length = array.length
-
-  if length <=1
-    return array
-  end
-
-  # puts "--> ARRAY: #{array.inspect}"
-
-  half = length/2
-  index = half.to_i
-  value = array[index]
-  array.delete_at(index)
-  left = []
-  right = []
-
-  array.each do |x|
-    if x <= value
-      left.push(x)
-    else
-      right.push(x)
-    end
-  end
-
-  # puts "--> PIVOT: #{value}"
-  # puts "------> LEFT: #{left.inspect}"
-  # puts "------> RIGHT: #{right.inspect}"
-
-  return quickSort(left) + [value] + quickSort(right)
-
-end
-
-def radixSort(array)
-  array_copy = array
-
-  numbers_of_passes = 0
-
-  array.each do |x|
-    num = x.abs.to_s.size
-    if num > numbers_of_passes
-      numbers_of_passes = num
-    end
-  end
-
-  numbers_of_passes.times do |i|
-    bucket = Array.new(20) {[]}
-    puts bucket.inspect
-    # bucket[1] << array_copy
-    # puts bucket.inspect
-    array.each do |n|
-      # bucket_index = bucket[n]
-      # puts bucket_index
-      z = nil
-      k = n.to_s.split('').map { |digit| z = digit.to_i }
-      if n < 0 then
-        z = -z;
+  def insertionSort
+    array = self
+    (1...array.length).each do |x|
+      value = array.delete_at(x)
+      index = x
+      while index > 0 && value < array[index - 1]
+        index -= 1
       end
-      bucket[z] = [z]
-      puts bucket.flatten
-      puts bucket.inspect
-      #puts "NUMBER: #{n} LAST: #{z}"
+      array.insert(index, value)
+    end
+    return array
+  end
 
-
-      # k = n.to_s.split('').map { |digit| digit.to_i }
-      # puts k
-      # bucket.flatten
-      # puts bucket.inspect
+  def mergeSort
+    array = self
+    length = array.length
+    if length <= 1
+      return array
+    else
+      x = length/2
+      left = (array[0...x]).mergeSort
+      right = (array[x..length]).mergeSort
+      merge(left, right)
     end
   end
 
-  return array_copy
+  def merge(left, right)
+    array2 = []
+    until left.empty? == true || right.empty? == true
+      if left[0] <= right[0]
+        array2.push(left.shift)
+      else
+        array2.push(right.shift)
+      end
+    end
+    array2 + left + right
+  end
+
+  def quickSort
+    array = self
+    length = array.length
+
+    if length <=1
+      return array
+    end
+
+    half = length/2
+    index = half.to_i
+    value = array[index]
+    array.delete_at(index)
+    left = []
+    right = []
+
+    array.each do |x|
+      if x <= value
+        left.push(x)
+      else
+        right.push(x)
+      end
+    end
+
+    return left.quickSort + [value] + right.quickSort
+  end
+
+  def radixSort
+    array_copy = self
+
+    numbers_of_passes = 0
+
+    array_copy.each do |x|
+      num = x.abs.to_s.size
+      if num > numbers_of_passes
+        numbers_of_passes = num
+      end
+    end
+
+    numbers_of_passes.times do |i|
+      buckets = Array.new(20) { [] }
+      num = 10**i
+
+      array_copy.each do |n|
+        bucket_index = (n/num) % 10
+        if 0 <= n
+          bucket_index += 10
+        end
+        buckets[bucket_index].push(n)
+      end
+
+      array_copy = buckets.flatten
+    end
+    return array_copy
+  end
+
 end
 
-a = [3, -6, 38, -666, 666, 0]
-b = [-3, 3, 334, -232312, 2, 0]
-y = [3, 43, 6, -9, 0, -300, 239, 49, -234, -123456789, 123456789012]
-x = [9, -7, 0, 5]
-puts quickSort(a).inspect
-
-
-# test = Array.new(100000) { rand(1000) }
-
-# # uno = mergeSort(test).inspect
-# # two = insertionSort(test).inspect
-
-# # if uno == two
-# #   puts "YES"
-# # else
-# #   puts "NO"
-# # end
-
-# require 'benchmark'
-# puts "MERGE SORT"
-# puts Benchmark.measure { mergeSort(test) }
-# puts "INSERTION SORT"
-# puts Benchmark.measure { insertionSort(test) }
-# puts "QUICK SORT"
-# puts Benchmark.measure { quickSort(test) }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def bogo(array)
-#   until array == array.sort
-#     if array != array.sort
-#       array.shuffle
-#     end
-#   end
-# end
--- test/sort_test.rb
+++ test/sort_test.rb
@@ -12,43 +12,52 @@ describe "test sort methods" do
   end
 
   it "should sort the numbers from least to greatest using insertion sort" do
-    y = insertionSort(@x)
+    y = @x.insertionSort
     y.must_equal [-300, -234, -9, 0, 0, 3, 6, 43, 239, 3329, 33423]
   end
 
   it "should sort the numbers from least to greatest using merge sort" do
-    y = mergeSort(@x)
+    y = @x.mergeSort
     y.must_equal [-300, -234, -9, 0, 0, 3, 6, 43, 239, 3329, 33423]
   end
 
   it "should sort the numbers from least to greatest using quick sort" do
-    y = quickSort(@x)
+    y = @x.quickSort
     y.must_equal [-300, -234, -9, 0, 0, 3, 6, 43, 239, 3329, 33423]
   end
 
   it "lets try random numbers :]" do
-    y = insertionSort(@x2)
-    x = mergeSort(@x2)
+    y = @x2.insertionSort
+    x = @x2.mergeSort
     z = @x2.sort
     y.must_equal x
     x.must_equal z
   end
 
   it "should fail on empty array" do
-    x = mergeSort(@x3)
+    x = @x3.mergeSort
     x.must_equal []
+  end
+
+  it "should sort the numbers from least to greatest using radix sort" do
+    y = @x.radixSort
+    y.must_equal [-300, -234, -9, 0, 0, 3, 6, 43, 239, 3329, 33423]
   end
 
 end
 
 Minitest.after_run{
-  test = Array.new(10000) { rand(1000) }
+  test = Array.new(100000) { rand(100000000000000) }
 
   puts "MERGE SORT"
-  puts Benchmark.measure { mergeSort(test) }
+  puts Benchmark.measure { test.mergeSort }
   puts "INSERTION SORT"
-  puts Benchmark.measure { insertionSort(test) }
+  puts Benchmark.measure { test.insertionSort }
   puts "QUICK SORT"
-  puts Benchmark.measure { quickSort(test) }
+  puts Benchmark.measure { test.quickSort }
+  puts "RADIX SORT"
+  puts Benchmark.measure { test.radixSort }
+  puts "THE .SORT"
+  puts Benchmark.measure { test.sort }
 }
 
